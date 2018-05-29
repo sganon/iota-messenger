62ec6d3edca3795339ddad31def4d8362f962726
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2018-05-29 12:44:28 +0200

    interface

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'vue app loaded'
+    iota: {},
+    node: {},
+    account: {},
+    message: {}
+  },
+  beforeCreate: function() {
+    
+    this.iota = new IOTA({
+      provider: 'http://nodes.iota.fm:80'
+    });
+    
+    this.iota.api.getNodeInfo((error, success) => {
+      if (error) {
+        alert(error);
+      } else {
+        this.node = success;
+      }
+    });
+    
+    this.message = {
+      type: 'message'
+    }
+    
+  },
+  methods: {
+    insertSeed: function() {
+      this.account = {
+        seed: prompt('Please insert yout IOTA seed to log in')
+      }
+    },
+    sendMessage: function() {
+      const value = document.querySelector('#message').value;
+      console.log(`send message: ${value}`);
+    },
+    sendPayment: function() {
+      const value = prompt('how much IOTA do you want to send ?');
+      if (value >= this.account.balance) {
+        console.log(`send ${value} IOTA`);
+      } else {
+        alert('insufficient balance');
+      }
+    }
   }
 });
-
-console.log('test');
-
-const iota = new IOTA({
-  provider: 'http://nodes.iota.fm:80'
-});
-
-iota.api.getNodeInfo((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(success);
-  }
-})
